eef5059f330766b0b73678cfc66d381480010603
Mwansa Pembanyali <mwansap07@gmail.com>
2020-07-13 00:16:35 +0200

    feat: Added Bootstrapper for iOS

---
+++ DoToo.iOS/Bootstrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DoToo.iOS
+{
+    public class Bootstrapper
+    {
+        public static void Init()
+        {
+            var instance = new Bootstrapper();
+        }
+    }
+}
