43470caec2c0674619e16cad776ac34a72ed40bd
Mwansa Pembanyali <mwansap07@gmail.com>
2020-07-13 01:54:42 +0200

    feat: Added Command for Navigation and Binding.

--- DoToo/ViewModels/MainViewModel.cs
+++ DoToo/ViewModels/MainViewModel.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Threading.Tasks;
 using DoToo.Repositories;
+using System.Windows.Input;
+using DoToo.Views;
+using Xamarin.Forms;
 
 namespace DoToo.ViewModels
 {
     public class MainViewModel : ViewModel
     {
+        public ICommand AddItem => new Command(async () =>
+        {
+            var itemView = Resolver.Resolve<ItemView>();
+            await Navigation.PushAsync(itemView);
+        });
+
         private readonly TodoItemRepository repository;
 
         public MainViewModel(TodoItemRepository repository)
