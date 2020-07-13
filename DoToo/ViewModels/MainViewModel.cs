c757b646c91399669cea6c1428ef0c046ff9161f
Mwansa Pembanyali <mwansap07@gmail.com>
2020-07-14 00:40:50 +0200

    feat: Added data binding to ItemView page. Added Observable collection for todo items. Added wrapper method to add an event and property to standard TodoItem class. Added events for tracking changes to repository list data.

--- DoToo/ViewModels/MainViewModel.cs
+++ DoToo/ViewModels/MainViewModel.cs
@@ -4,11 +4,16 @@ using DoToo.Repositories;
 using System.Windows.Input;
 using DoToo.Views;
 using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using System.Linq;
+using DoToo.Models;
 
 namespace DoToo.ViewModels
 {
     public class MainViewModel : ViewModel
     {
+        public ObservableCollection<TodoItemViewModel> Items { get; set; }
+
         public ICommand AddItem => new Command(async () =>
         {
             var itemView = Resolver.Resolve<ItemView>();
@@ -19,13 +24,31 @@ namespace DoToo.ViewModels
 
         public MainViewModel(TodoItemRepository repository)
         {
+            repository.OnItemAdded += (sender, item) =>
+                Items.Add(CreateTodoItemViewModel(item));
+            repository.OnItemUpdated += (sender, item) =>
+                Task.Run(async () => await LoadData());
+
             this.repository = repository;
             Task.Run(async () => await LoadData());
         }
 
         private async Task LoadData()
         {
+            var items = await repository.GetItems();
+            var itemViewModels = items.Select(i => CreateTodoItemViewModel(i));
+            Items = new ObservableCollection<TodoItemViewModel>(itemViewModels);
+        }
 
+        private TodoItemViewModel CreateTodoItemViewModel(TodoItem item)
+        {
+            var itemViewModel = new TodoItemViewModel(item);
+            itemViewModel.ItemStatusChanged += ItemStatusChanged;
+            return itemViewModel;
+        }
+
+        private void ItemStatusChanged(object sender, EventArgs e)
+        {
         }
     }
 }
